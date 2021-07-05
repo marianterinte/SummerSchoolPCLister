79d6f8b633c7643526ebb55c83b45583ed258b97
Marian Terinte <marian.terinte@gmail.com>
2021-07-05 18:58:09 +0300

    Project Started.

---
+++ PCLister/PCLister/PC.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCLister
+{
+	public class PC
+	{
+		public string motherboardMake;
+		public string processorMake;
+		public string processorFrequency;
+		public string ramSize;
+		public string ramFrequency;
+
+		public PC(string motherboardMake, string processorMake, string processorFrequency, string ramSize, string ramFrequency)
+		{
+			this.motherboardMake = motherboardMake;
+			this.processorMake = processorMake;
+			this.processorFrequency = processorFrequency;
+			this.ramSize = ramSize;
+			this.ramFrequency = ramFrequency;
+		}
+	}
+}
--- PCLister/PCLister/Program.cs
+++ PCLister/PCLister/Program.cs
@@ -7,7 +7,36 @@ namespace PCLister
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("PC Type: ");
+			var pcType = Console.ReadLine();
+
+			Console.WriteLine("Processor: ");
+			var processorMake = Console.ReadLine();
+
+			Console.WriteLine("Display: ");
+			var format = Console.ReadLine();
+
+			var pc = new PC("Asus", "AMD", "4Ghz", "16GB", "3600Mhz");
+
+			Console.WriteLine("Motherboard: " + pc.motherboardMake);
+			Console.WriteLine("Processor: " + pc.processorMake);
+			Console.WriteLine("Processor Freq: " + pc.processorFrequency);
+			Console.WriteLine("Ram Size: " + pc.ramSize);
+			Console.WriteLine("Ram Frequency: " + pc.ramFrequency);
+
+			//basic
+			//AMD
+			//min
+
+			// Motherboard: Asus
+			// Processor: AMD
+			// Processor Freq: 4Ghz
+			// Ram Size: 16GB
+			// Ram Freq: 3600 Mhz
+
+
+
+
 		}
 	}
 }
